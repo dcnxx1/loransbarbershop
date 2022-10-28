3caad9034e53b6e034a8dbfc19e164a353e99e89
dcnxx1 <alkisali1@hotmail.com>
2022-10-28 22:00:04 +0200

    started with nav

--- app.js
+++ app.js
@@ -6,13 +6,30 @@ app.set('views', path.join(__dirname, './src/pages'))
 app.set('view engine', 'ejs')
 app.use('/css', express.static(path.join(__dirname, './src/css')))
 app.use('/js', express.static(path.join(__dirname, './src/js')))
-
+app.use('/assets', express.static(path.join(__dirname, './src/assets')))
 
 
 
 app.get('/', (req,res) => {
     res.render('index')
 })
+
+app.get('/over', (req,res) => {
+    res.render('about')
+})
+
+app.get('/contact', (req,res) => {
+    res.render('contact')
+})
+
+app.get('/tarieven', (req,res) => {
+    res.render('prices')
+})
+
+app.get('*', (req,res) => {
+    res.send("verdwaald?")
+})
+
 
 
 app.listen(3000, () => {
