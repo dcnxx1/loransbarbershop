f235327276e82a10ce0985dce86ddb7fd366931d
dcnxx1 <alkisali1@hotmail.com>
2022-10-30 21:49:59 +0100

    nav done

---
+++ src/js/script.js
@@ -0,0 +1,63 @@
+
+const menuBurger = document.querySelector('.line-container')
+const mobileMenu = document.querySelector('.mobile-menu')
+const header = document.getElementById('header')
+
+
+
+
+ // hides mobile menu
+function hideMobileMenu() {
+
+    if(mobileMenu.classList.contains('hide-initial')){
+        mobileMenu.classList.add('show')
+        mobileMenu.classList.remove('hide-initial')
+    } else {
+        if(mobileMenu.classList.contains('hide')){
+            mobileMenu.classList.add('show')
+            mobileMenu.classList.remove('hide')
+        } else {
+            mobileMenu.classList.add('hide')
+            mobileMenu.classList.remove('show')
+        }
+    }
+
+}
+
+window.onresize =  () => {
+    if(window.outerWidth > 768){
+        menuBurger.querySelectorAll('.line-long').forEach(obj => {
+            obj.classList.remove('normal-menu')
+            obj.classList.remove('x-menu-from')
+            obj.classList.add('line-container')
+        })
+        menuBurger.style.display = "none"
+        mobileMenu.classList.remove('show')
+        mobileMenu.classList.add('hide')
+    } else {
+        menuBurger.style.display = "block"
+    }
+}
+
+
+
+// converts to closeable menu 
+function convertX() {
+    const childClass = menuBurger.querySelectorAll('.line-long')
+    childClass.forEach(obj => {
+       if(obj.classList.contains('x-menu-from') === false){
+        obj.classList.add('x-menu-from')
+        obj.classList.remove('normal-menu')
+       } else {
+        obj.classList.remove('x-menu-from')
+        obj.classList.add('normal-menu')
+       }
+    })
+}
+
+
+
+menuBurger.addEventListener('click', e => {
+   hideMobileMenu()
+   convertX()
+})
